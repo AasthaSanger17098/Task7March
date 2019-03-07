12f1a2a233a50b2aa64de42a56d1b40470fd525a
AasthaSanger17098 <aastha.sanger_mca18@gla.ac.in>
2019-03-07 11:59:13 +0530

    All Done v1.0.0

--- app/src/main/java/com/gla/task7march/MainActivity.java
+++ app/src/main/java/com/gla/task7march/MainActivity.java
@@ -3,6 +3,7 @@ package com.gla.task7march;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -11,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
     EditText editText3;
     EditText editText4;
     Button button;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,30 +19,36 @@ public class MainActivity extends AppCompatActivity {
         editText3 = findViewById(R.id.editText3);
         editText4 = findViewById(R.id.editText4);
         button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = editText3.getText().toString().trim();
+                String pass = editText4.getText().toString().trim();
+                SharedPreferences sharedPreferences = getSharedPreferences("com.gla.task7march",MODE_PRIVATE);
+                String s_user=sharedPreferences.getString("user", "user");
+                String s_pass=sharedPreferences.getString("pass","");
+                if(user.equals(s_user)&& pass.equals(s_pass))
+                {
+                    Toast.makeText(MainActivity.this, "Passed", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        SharedPreferences sharedPreferences = getSharedPreferences("com.gla.task7march",MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("aastha.sanger+mca18@gla.ac.in",editText3.getText().toString().trim());
-        editor.putString("123456",editText4.getText().toString().trim());
-        editor.putBoolean("bool",true);
-        editor.apply();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         SharedPreferences sharedPreferences = getSharedPreferences("com.gla.task7march",MODE_PRIVATE);
-        String s1 = sharedPreferences.getString("aastha.sanger+mca18@gla.ac.in","UserName");
-        String s2 = sharedPreferences.getString("123456","Password");
-        Boolean remember = sharedPreferences.getBoolean("bool",false);
-        Toast.makeText(this, "bool: "+remember, Toast.LENGTH_SHORT).show();
-        editText3.setText(s1);
-        editText4.setText(s2);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("user","aastha.sanger_mca18@gla.ac.in");
+        editor.putString("pass","12345");
+        editor.apply();
 
     }
+
 }
